6d45984ffa1c7f79a1c44742685d1df81c79256c
Vinay Hosamane K N <vinay.k.n.hosamane@sap.com>
2019-06-06 09:31:42 +0530

    Added problem description to Kata 2

--- CodeWar_Kata's/CodeWars_Kata2.playground/Contents.swift
+++ CodeWar_Kata's/CodeWars_Kata2.playground/Contents.swift
@@ -1,6 +1,23 @@
 import UIKit
 import Foundation
 
+/*
+Description:
+Encrypt this!
+
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter needs to be converted to its ASCII code.
+The second letter needs to be switched with the last letter
+Keepin' it simple: There are no special characters in input.
+Examples:
+encryptThis "Hello" == "72olle"
+encryptThis "good" == "103doo"
+encryptThis "hello world" == "104olle 119drlo"
+ */
+
 //For Swift 4
 //extension Character {
 //    var ascii: UInt32? {
