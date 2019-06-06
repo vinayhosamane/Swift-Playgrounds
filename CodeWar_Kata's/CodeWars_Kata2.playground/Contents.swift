827c579cbe4b0a588f7e198cce047bf03c7bd2e0
Vinay Hosamane K N <vinay.k.n.hosamane@sap.com>
2019-06-06 09:29:53 +0530

    Solved Kata from Code Wars

---
+++ CodeWar_Kata's/CodeWars_Kata2.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+import Foundation
+
+//For Swift 4
+//extension Character {
+//    var ascii: UInt32? {
+//        return unicodeScalars.first?.value
+//    }
+//}
+// Swift 5 has new api for getting ASCII value
+//func encryptThis(text: String) -> String {
+//    // Your awesome code goes here!
+//    let allWords = text.components(separatedBy: " ")
+//    let result = allWords.map { (word) -> String in
+//        return applySequenceOfCipherRules(word)
+//    }.joined(separator: " ")
+//    return result
+//}
+
+func encryptThis(text:String) -> String {
+    return text
+        .components(separatedBy: " ")
+        .map {
+            var a = $0.map { String($0) }
+            a[0] = String(a[0].unicodeScalars.first!.value)
+            
+            if a.count > 2 {
+                a.swapAt(1, a.count-1)
+            }
+            
+            return a.joined()
+        }
+        .joined(separator: " ")
+}
+
+func applySequenceOfCipherRules(_ text: String) -> String {
+    //Replace first character by ascii value
+    //Swap second character with last character
+    var result = text
+    let secondIndex = text.index(text.startIndex, offsetBy: 1)
+    let lastIndex = text.index(text.endIndex, offsetBy: -1)
+    
+    result.swap(at: secondIndex, to: text[lastIndex])
+    result.swap(at: lastIndex, to: text[secondIndex])
+    
+//    let temp = text[secondIndex]
+//    text[secondIndex] = text[lastIndex]
+//    text[secondIndex] = temp
+    
+//    text.enumerated().forEach { (index, char) in
+//        if index == 0 {
+//            guard let ascii = char.asciiValue else {
+//              return
+//            }
+//            result.append(String(ascii))
+//        } else if index == 1 {
+//            result.append(text[lastIndex])
+//        } else if index == text.count - 1 {
+//            result.append(text[secondIndex])
+//        } else {
+//            result.append(char)
+//        }
+//    }
+    
+    return result
+}
+
+extension String {
+    mutating func swap(at index: String.Index, to character: Character) {
+        let endIndex = self.index(after: index)
+        let range = index ..< endIndex
+        assert(indices.contains(index) && indices.contains(endIndex))
+        replaceSubrange(range, with: String(character))
+    }
+}
+
+print(encryptThis(text: "hello world"))
